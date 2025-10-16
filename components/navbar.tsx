e590ff5dc16eb401e957bff587c56d152b158fd7
Abel Hirwa <hirwaabel1738@gmail.com>
2025-10-16 20:44:17 +0200

    Started creating  my ecommerce website

---
+++ components/navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+    return ( 
+        <nav> 
+            <div>
+                <Link href="/"> My Ecommerce </Link>
+            </div> 
+        
+        </nav>
+        
+    )
+}
