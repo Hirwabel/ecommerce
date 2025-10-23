d4cc928262b1aab729b4551775291a02160eb5fa
Abel Hirwa <hirwaabel1738@gmail.com>
2025-10-23 06:18:03 +0200

    Added some styling on the product list and the product card components  and wrote a few lines for searching/filtering products

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,6 +2,7 @@ import Stripe from "stripe";
 import Link from "next/link"
 import Image from "next/image"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 interface Props {
     product: Stripe.Product
@@ -12,26 +13,31 @@ export const ProductCard = ({ product }: Props) => {
   const price = product.default_price as Stripe.Price;
     return (
       <Link href={"/products/1"}>
-        <Card>
+        <Card className="group hover:shadow-2xl transition duration-300 py-0 h-full flex flex-col border-gray-300 gap-0">
           {product.images && product.images[0] && (
               <div className="relative h-80 w-full bg-gray-100 flex items-center justify-center">
                 <Image
                   src={product.images[0]}
                   alt={product.name}
-                  fill
-                  className="object-contain transition-transform duration-500 ease-in-out"
+                  layout="fill"
+                  /*objectFit="cover"*/
+                  className="group-hover:opacity-90 transition-opacity duration-300 rounded-t-lg"
                 />
               </div>
             )}
 
-            <CardHeader> 
-              <CardTitle> {product.name} </CardTitle>
-              <CardContent> 
+            <CardHeader className="p-4"> 
+              <CardTitle className="text-xl font-bold text-gray-800"> {product.name} </CardTitle>
+              <CardContent className="p-4 flex-grow flex flex-col justify-between"> 
+                {product.description && (
+                  <p className="text-gray-600 text-sm mb-2">{product.description}</p>
+                )}
                 {price && price.unit_amount && (
-                  <p className="text-xl text-white">
-                    ${(price.unit_amount / 100).toFixed(2)}
+                  <p className="text-lg font-semibold text-gray-900">
+                  ${(price.unit_amount / 100).toFixed(2)}
                   </p>
-                )} 
+                )}
+              <Button className="mt-4 bg-black text-white">View Details</Button>
               </CardContent> 
             </CardHeader>
         </Card>
