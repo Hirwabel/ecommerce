02c8f3f688fdb518b749385c8a6f052023f226fa
Abel Hirwa <hirwaabel1738@gmail.com>
2025-10-31 06:36:24 +0200

    Added an order summary for when users click to checkout after shopping

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,3 +1,34 @@
+"use client";
+import { useCartStore } from "@/store/cart-store";
+import { Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+
 export default function CheckoutPage () {
-    return <div>checkout</div>;
+    const { items } = useCartStore();
+    const total = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    if (total === 0 || items.length === 0){
+        return <div><h1> Your Cart is Empty.</h1></div>
+    }
+    return (
+      <div>
+        <h1> Checkout </h1>
+        <Card>
+            <CardHeader>
+                <CardTitle> Order Summary </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <ul>
+                    {items.map((item) => (
+                        <li key={item.id}>
+                            <div>
+                                <span> {item.name}</span>
+                                <span> {((item.price * item.quantity) / 100).toFixed(2)} </span>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </CardContent>
+        </Card>
+        
+      </div>
+    )  
 }
