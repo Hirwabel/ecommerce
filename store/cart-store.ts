a26d9fa7e0449b6a1caca9390a57d757c26b310a
Abel Hirwa <hirwaabel1738@gmail.com>
2025-11-01 20:47:42 +0200

    Finishing the tutorial but stripe doesn't work here so i'm still looking for a way around

--- store/cart-store.ts
+++ store/cart-store.ts
@@ -13,6 +13,8 @@ export interface CartItem {
 interface CartStore { 
     items: CartItem[]; 
     addItem: (item : CartItem) => void; 
+    removeItem: (id: string) => void;
+    clearCart: () => void;
 }
 
 export const useCartStore = create<CartStore>()(
